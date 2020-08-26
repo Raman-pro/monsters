96a710d1aad508801e42cbbeb026599f8e0f37e0
Ramanjot Singh <ramanjot2007@outlook.com>
2020-08-26 16:38:43 +0530

    Updates

---
+++ precache-manifest.ba4d8229c1451118eae898557fd1b63b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff6d785988c4791ce97222ced55dbdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ca43fd317edcb6c947",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f99880cc38fe5fc38d34",
+    "url": "/static/js/2.cf05e8cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf05e8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ca43fd317edcb6c947",
+    "url": "/static/js/main.840bb496.chunk.js"
+  },
+  {
+    "revision": "18a0fb973d709ca4c1e3",
+    "url": "/static/js/runtime-main.38ff4272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
